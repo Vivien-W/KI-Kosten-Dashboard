311234a39d47216d9507a5738136a4210862f72d
Vivien Wallbaum <viviwbaum@gmail.com>
2026-02-03 15:32:27 +0100

    Datenbankfix für Deployment

--- backend/dist/db.js
+++ backend/dist/db.js
@@ -1,9 +1,19 @@
-// src/db.ts
 import { Pool } from "pg";
+import dotenv from "dotenv";
 
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+dotenv.config();
+
+const isRender = !!process.env.DATABASE_URL;
+
+export const pool = isRender
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    })
+  : new Pool({
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      host: process.env.PGHOST,
+      port: Number(process.env.PGPORT),
+      database: process.env.PGDATABASE,
+    });
